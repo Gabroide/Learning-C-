4b2328d55b5885b77bd048f7a6b51b2155aebd2f
_gaspode <gaspode@uoc.edu>
2019-01-27 17:00:21 +0100

    Examples for chapter 3

---
+++ Chapter03/GreaterThenOrEqualToOperator.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	std::cout << "Enter your fisrt number: " << std::endl;
+	int number1 = 0;
+	std::cin >> number1;
+
+	std::cout << "Enter your second number: " << std::endl;
+	int number2 = 0;
+	std::cin >> number2;
+
+	bool result = number1 >= number2;
+	std::cout << "It's "
+		<< result
+		<< " that your first number is greater than or equal to your second."
+		<< std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Enter your fisrt number: " << std::endl;
+	std::cin >> number1;
+
+	std::cout << "Enter your second number: " << std::endl;
+	std::cin >> number2;
+
+	result = number1 >= number2;
+	std::cout << "It's "
+		<< result
+		<< " that your first number is greater than or equal to your second."
+		<< std::endl;
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "Enter your fisrt number: " << std::endl;
+	std::cin >> number1;
+
+	std::cout << "Enter your second number: " << std::endl;
+	std::cin >> number2;
+
+	result = number1 >= number2;
+	std::cout << "It's "
+		<< result
+		<< " that your first number is greater than or equal to your second."
+		<< std::endl;
+
+	return 0;
+}
