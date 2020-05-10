d2fc9b2ef21d66a17b9b716975087093c592abbc
Nosv <nosv@hotmail.com>
2020-05-10 22:16:47 +0200

    Piensa en C++ axamples and excercises from chapter 3

---
+++ Piensa en C++/Vol1/C03/ArgsToInts.cpp
@@ -0,0 +1,15 @@
+//: C03:ArgsToInt.cpp
+// Converting command-line arguments to ints
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	for(int i=0; i<argc; i++)
+	{
+		cout << atoi(argv[i]) << endl;
+	}
+} ///:~
